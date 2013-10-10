f91d27a157d537fd79840fbce4ba122c4f4200e0
snapshotpl <witold@wasiczko.pl>
2013-10-10 11:43:42 +0200

    Move Module.php and improvments in composer.json

--- Module.php
+++ Module.php
@@ -1,3 +1,36 @@
 <?php
 
-require __DIR__ .'/src/ZfSnapJquery/Module.php';
+namespace ZfSnapJquery;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+/**
+ * Module
+ *
+ * @author Witold Wasiczko <witold@wasiczko.pl>
+ */
+class Module implements ConfigProviderInterface, AutoloaderProviderInterface
+{
+    /**
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
